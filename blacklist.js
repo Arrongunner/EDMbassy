97735b0a15e4928a19d8e32c62760b1a91db4f46
NitroGhost <wildeman.benjamin@gmail.com>
2013-06-30 09:19:36 +0100

    Update blacklist.js

--- blacklist.js
+++ blacklist.js
@@ -15,8 +15,8 @@ function checkBlacklist(user) {
 API.addEventListener(API.CHAT, yolosticles);
 
 function yolosticles(data) {
-        var replies = ["oh, mah gawd, like totes YOLO!", "YOLO ma HOLO!", "YOLO: You only love once!"];
-		if (data.message.indexOf('yolo') > -1 || data.message.indexOf('YOLO') > -1) {
-            API.sendChat("@" + data.from + " " + replies[Math.floor(Math.random() * replies.length)]);
-		}
+        var yMsg = ["oh, mah gawd, like totes YOLO!", "YOLO ma HOLO!", "YOLO: You only love once!"];
+	if (data.message.indexOf('yolo') > -1) {
+        	API.sendChat("@" + data.from + " " + yMsg[Math.floor(Math.random() * yMsg.length)]);
+	}
 }
