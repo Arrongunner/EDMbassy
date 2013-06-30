ad13defbcf45f64d9866b84b9705a93546d7e500
NitroGhost <wildeman.benjamin@gmail.com>
2013-06-30 10:04:35 +0100

    Update blacklist.js

--- blacklist.js
+++ blacklist.js
@@ -1,17 +1,3 @@
-blacklist = [
-	 "50aeb489d6e4a94f7747d4a8",
-]
-	 
-
-API.addEventListener(API.USER_JOIN, checkBlacklist);
-
-function checkBlacklist(user) {
-        if (blacklist.indexOf(user.id) > -1 ) {
-                API.sendChat("/me automated kick");
-                API.moderateBanUser(user.id, "HEH HEH HEH HEH I told you I'd have the last laugh");
-        }
-}
-
 var ohmahgawded = false;
 var ohmahgawdedWait = 300000;
 var ohmahgawdedPassed = 0;
